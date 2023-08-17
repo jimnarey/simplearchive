29ccb0db8afa653ce2c610ad92f8bc401ca900d7
Jim Narey <jimnarey@me.com>
2023-08-17 13:45:02 +0100

    Experiment with opening different archive types

--- archive/archive.py
+++ archive/archive.py
@@ -1,29 +1,109 @@
 #!/usr/bin/env python3
 
 import os
+import pathlib
+import shutil
 import zipfile
 import tarfile
 import gzip
 import bz2
 import lzma
+from typing import IO,Optional
 
 import py7zr
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES = os.path.join(SCRIPT_DIR, '../tests/fixtures')
 OUT =  os.path.join(SCRIPT_DIR, '../out')
+OUT_FILE = os.path.join(OUT, 'file.txt')
 
-with zipfile.ZipFile(os.path.join(FIXTURES, 'file.zip'), 'r') as zipf:
-    # Extract all contents to the specified folder
-    zipf.extractall(OUT)
+def print_file(path):
+    with open(path, 'r') as file:
+        print(file.read())
 
-TARS = ['file.tar.bz2', 'file.tar.gz', 'file.tar.xz']
 
-for tarname in TARS:
+def all_files(path: str) -> list[str]:
+    root_dir = pathlib.Path(path)
+    return [item for item in root_dir.rglob('*') if item.is_file()]
+
+archives = all_files(FIXTURES)
+
+def open_as_zip(fileobj: IO[bytes]) -> Optional[zipfile.ZipFile]:
+    try:
+        zipf = zipfile.ZipFile(fileobj)
+    except zipfile.BadZipFile:
+        return None
+    return zipf
+
+def open_as_tar(fileobj: IO[bytes]) -> Optional[zipfile.ZipFile]:
+    try:
+        tarf = tarfile.open(fileobj=fileobj)
+    except Exception as e:
+        print(e)
+        return None
+    return tarf
+
+def open_as_gzip(fileobj: IO[bytes]) -> Optional[zipfile.ZipFile]:
+    try:
+        gzipf = gzip.open(fileobj)
+        gzipf.peek()
+    except Exception as e:
+        print(type(e))
+        return None
+    return gzipf
+
+def open_as_bz2(fileobj: IO[bytes]) -> Optional[zipfile.ZipFile]:
+    try:
+        bz2f = bz2.open(fileobj)
+        bz2f.peek()
+    except Exception as e:
+        print(type(e))
+        return None
+    return bz2f
+
+# for archive in archives:
+#     with open(archive, 'rb') as arch_file:
+#         open_as_tar(arch_file)
+
+SUPPORTED_TARS = ['file.tar.bz2', 'file.tar.gz', 'file.tar.xz']
+
+for tarname in SUPPORTED_TARS:
     with tarfile.open(os.path.join(FIXTURES, tarname)) as tarf:
         tarf.extractall(path=OUT)
+    # print_file(OUT_FILE)
+    # os.remove(OUT_FILE)
 
+with open(os.path.join(OUT, 'file.txt'), 'wb') as out_file:
+    with gzip.open(os.path.join(FIXTURES, 'file.txt.gz')) as gzf:
+        shutil.copyfileobj(gzf, out_file)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
 
+with open(os.path.join(OUT, 'file.txt'), 'wb') as out_file:
+    with bz2.open(os.path.join(FIXTURES, 'file.txt.bz2')) as bzf:
+        shutil.copyfileobj(bzf, out_file)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
 
+with open(os.path.join(OUT, 'file.txt'), 'wb') as out_file:
+    with lzma.open(os.path.join(FIXTURES, 'file.txt.xz')) as bzf:
+        shutil.copyfileobj(bzf, out_file)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
 
+with zipfile.ZipFile(os.path.join(FIXTURES, 'file.txt.zip'), 'r') as zipf:
+    zipf.extractall(OUT)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
 
+with py7zr.SevenZipFile(os.path.join(FIXTURES, 'file.txt.7z'), 'r') as szf:
+    szf.extractall(path=OUT)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
+
+with py7zr.SevenZipFile(os.path.join(FIXTURES, 'file.tar.7z'), 'r') as sztarf:
+    contents = sztarf.readall()
+    with tarfile.open(fileobj=contents['file.tar']) as tarf:
+        tarf.extractall(path=OUT)
+# print_file(OUT_FILE)
+# os.remove(OUT_FILE)
---
+++ tests/test_archive.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+from zipfile import ZipFile
+from tarfile import TarFile
+from gzip import GzipFile
+from bz2 import BZ2File
+from archive import archive
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_DIR = os.path.join(SCRIPT_DIR, 'fixtures')
+
+class TestArchive(unittest.TestCase):
+
+    def test_open_as_zip_with_zip_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.zip'), 'rb') as fileobj:
+            value = archive.open_as_zip(fileobj)
+            self.assertIsInstance(value, ZipFile)
+
+    def test_open_as_zip_with_non_zip_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.bz2'), 'rb') as fileobj: 
+            value = archive.open_as_zip(fileobj)
+            self.assertIsNone(value)
+
+    def test_open_as_tar_with_tar_bz2_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.tar.bz2'), 'rb') as fileobj: 
+            value = archive.open_as_tar(fileobj)
+            self.assertIsInstance(value, TarFile)
+
+    def test_open_as_tar_with_tar_gz_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.tar.gz'), 'rb') as fileobj: 
+            value = archive.open_as_tar(fileobj)
+            self.assertIsInstance(value, TarFile)
+
+    def test_open_as_tar_with_tar_xz_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.tar.xz'), 'rb') as fileobj: 
+            value = archive.open_as_tar(fileobj)
+            self.assertIsInstance(value, TarFile)
+
+    def test_open_as_tar_with_non_tar_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.zip'), 'rb') as fileobj: 
+            value = archive.open_as_tar(fileobj)
+            self.assertIsNone(value)
+
+
+    def test_open_as_bz2_with_bz2_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.bz2'), 'rb') as fileobj: 
+            value = archive.open_as_bz2(fileobj)
+            self.assertIsInstance(value, BZ2File)
+
+    def test_open_as_bz2_with_non_bz2_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.bak'), 'rb') as fileobj: 
+            value = archive.open_as_bz2(fileobj)
+            self.assertIsNone(value)
+
+    def test_open_as_gzip_with_gzip_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.gz'), 'rb') as fileobj: 
+            value = archive.open_as_gzip(fileobj)
+            self.assertIsInstance(value, GzipFile)
+
+    def test_open_as_gzip_with_non_gzip_file(self):
+        with open(os.path.join(FIXTURES_DIR, 'file.txt.zip'), 'rb') as fileobj: 
+            value = archive.open_as_gzip(fileobj)
+            self.assertIsNone(value)
+
